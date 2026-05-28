e155718241c940beb32154227c7c6bce7213c1d3
Ovavova <vyereschenko@gmail.com>
2026-05-28 08:46:54 +0300

    3-1

--- queries/3_1_aggregation_pipeline_top10.mongodb.js
+++ queries/3_1_aggregation_pipeline_top10.mongodb.js
@@ -1,10 +1,10 @@
-db.tracks.aggregate([
-  // Step 1: Розгортаємо масив артистів, щоб обробляти кожного виконавця окремо
+use('spotify'); 
+
+
+const topArtists = db.tracks.aggregate([
   { 
     $unwind: "$artists" 
   },
-  
-  // Step 2: Групуємо за виконавцем, рахуємо кількість треків та середню популярність
   {
     $group: {
       _id: "$artists",
@@ -12,32 +12,27 @@ db.tracks.aggregate([
       avgPopularity: { $avg: "$popularity" }
     }
   },
-  
-  // Step 3: Фільтруємо виконавців, які мають хоча б 5 треків
   {
     $match: {
       totalTracks: { $gte: 5 }
     }
   },
-  
-  // Step 4: Сортуємо за спаданням середньої популярності
   {
     $sort: {
       avgPopularity: -1
     }
   },
-  
-  // Step 5: Обмежуємо результат топ-10 виконавцями
   {
     $limit: 10
   },
-  
-  // Step 6: Форматуємо вивід (проекція), щоб залишити тільки потрібні поля
   {
     $project: {
       _id: 0,
       artist: "$_id",
-      avgPopularity: { $round: ["$avgPopularity", 2] } // Округлення для красивого виводу
+      avgPopularity: { $round: ["$avgPopularity", 2] }
     }
   }
-]);
+]).toArray(); // Конвертуємо у масив для виводу
+
+//Виводимо результат у Playground
+topArtists;
